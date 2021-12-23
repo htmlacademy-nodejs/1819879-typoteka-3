5d2db4d212c7d5d08b525466c2cd25869652c34b
Roman Shestakov <shestakov_rv@bk.ru>
2021-12-23 19:47:20 +0300

    Create cli module.

---
+++ constants.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports.DEFAULT_COMMAND = `--help`;
+
+module.exports.USER_ARGV_INDEX = 2;
+
+module.exports.DEFAULT_RADIX = 10;
+
+module.exports.ExitCode = {
+  SUCCESS: 0,
+  ERROR: 1,
+};
+
+module.exports.DEFAULT_QYANTITY = 1;
+module.exports.MAX_QYANTITY = 1000;
