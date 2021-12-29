1bbfb01665ff1a094fb98c0cc0d57656d46b26da
Roman Shestakov <shestakov_rv@bk.ru>
2021-12-29 23:35:54 +0300

    Added read data from files.

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -11,83 +11,20 @@ const DAY_INT = 24 * 3600 * 1000;
 const MAX_DAY_SUBTRACTION = 90;
 const FILE_NAME = `mock.json`;
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
+const DATA_PATH = `./data/`;
+const TITLES_FILE_NAME = `titles.txt`;
+const CATEGORIES_FILE_NAME = `categories.txt`;
+const SENTENCES_FILE_NAME = `sentences.txt`;
 
-const SENTENCES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
-
-const getRandomTitle = () => TITLES[getRandomInt(0, TITLES.length - 1)];
-
-const getRandomSentence = () => SENTENCES[getRandomInt(0, SENTENCES.length - 1)];
-const getRandomSentences = (maxQyantity) => Array(getRandomInt(0, maxQyantity))
-  .fill(null)
-  .map(() => getRandomSentence())
-  .join(` `);
-
-const getRandomCategory = () => CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)];
-const getRandomCategories = () => {
-  const categories = Array(getRandomInt(0, CATEGORIES.length - 1)).fill().map(() => getRandomCategory());
-  return Array.from(new Set(categories));
+const readFile = async (fileName) => {
+  try {
+    const content = await fs.readFile(`${DATA_PATH}${fileName}`, `utf8`);
+    return content.trim().split(`\n`);
+  } catch (error) {
+    console.error(chalk.red(`Write file failed... - ${error}`));
+    return ``;
+  }
 };
-
-const getRandomDate = () => {
-  const subtractionDaysInt = getRandomInt(0, MAX_DAY_SUBTRACTION) * DAY_INT;
-  const dateNowInt = new Date().getTime();
-  return new Date(dateNowInt - subtractionDaysInt);
-};
-
-const getRandomPost = () => ({
-  "title": getRandomTitle(),
-  "announce": getRandomSentences(MAX_ANNOUNCES),
-  "fullText": getRandomSentences(MAX_SENTENCES),
-  "createdDate": getRandomDate(),
-  "category": getRandomCategories()
-});
 
 const writeFile = async (content) => {
   try {
@@ -100,15 +37,51 @@ const writeFile = async (content) => {
   }
 };
 
+const getRandomTitle = (titles) => titles[getRandomInt(0, titles.length - 1)];
+
+const getRandomSentence = (sentences) => sentences[getRandomInt(0, sentences.length - 1)];
+const getRandomSentences = (sentences, maxQyantity) => Array(getRandomInt(0, maxQyantity))
+  .fill(null)
+  .map(() => getRandomSentence(sentences))
+  .join(` `);
+
+const getRandomCategory = (categories) => categories[getRandomInt(0, categories.length - 1)];
+const getRandomCategories = (categories) => {
+  const randomCategories = Array(getRandomInt(0, categories.length - 1)).fill().map(() => getRandomCategory(categories));
+  return Array.from(new Set(randomCategories));
+};
+
+const getRandomDate = () => {
+  const subtractionDaysInt = getRandomInt(0, MAX_DAY_SUBTRACTION) * DAY_INT;
+  const dateNowInt = new Date().getTime();
+  return new Date(dateNowInt - subtractionDaysInt);
+};
+
+const getRandomPost = (titles, categories, sentences) => ({
+  "title": getRandomTitle(titles),
+  "announce": getRandomSentences(sentences, MAX_ANNOUNCES),
+  "fullText": getRandomSentences(sentences, MAX_SENTENCES),
+  "createdDate": getRandomDate(),
+  "category": getRandomCategories(categories)
+});
+
+const getRandomPosts = async (count) => {
+  const titlesData = await readFile(TITLES_FILE_NAME);
+  const categoriesData = await readFile(CATEGORIES_FILE_NAME);
+  const sentencesData = await readFile(SENTENCES_FILE_NAME);
+
+  return new Array(count).fill(null).map(() => getRandomPost(titlesData, categoriesData, sentencesData));
+};
+
 module.exports = {
   name: `--generate`,
-  run(count = DEFAULT_QYANTITY) {
+  async run(count = DEFAULT_QYANTITY) {
     if (count > MAX_QYANTITY) {
       console.warn(chalk.red(`Не больше ${MAX_QYANTITY} публикаций`));
       process.exit(ExitCode.ERROR);
     }
 
-    const randomCategoriesJson = JSON.stringify(new Array(count).fill(null).map(() => getRandomPost()));
+    const randomCategoriesJson = JSON.stringify(await getRandomPosts(count));
     writeFile(randomCategoriesJson).then();
   }
 };
