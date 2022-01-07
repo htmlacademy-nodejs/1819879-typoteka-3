c3c62d7520b8f219b97507910a7645364abbbf4a
Roman Shestakov <shestakov_rv@bk.ru>
2022-01-07 16:36:38 +0300

    Create layout pages with pug.

--- src/express/express.js
+++ src/express/express.js
@@ -1,13 +1,17 @@
 'use strict';
 
 const chalk = require(`chalk`);
+const path = require(`path`);
 const express = require(`express`);
+
 const mainRouter = require(`./routes/main-routes`);
 const myRouter = require(`./routes/my-routes`);
-const articlesRouter = require(`./routes/articlesRoutes`);
+const articlesRouter = require(`./routes/articles-routes`);
 const {USER_ARGV_INDEX, DEFAULT_RADIX} = require(`../../constants`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
+const TEMPLATES_DIR = `templates`;
 
 const customPort = process.argv[USER_ARGV_INDEX];
 const port = Number.parseInt(customPort, DEFAULT_RADIX) || DEFAULT_PORT;
@@ -21,6 +25,10 @@ const onListenServer = (error) => {
 };
 
 const app = express();
+app.set(`views`, path.resolve(__dirname, TEMPLATES_DIR));
+app.set(`view engine`, `pug`);
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
 app.use(`/`, mainRouter);
 app.use(`/my`, myRouter);
 app.use(`/articles`, articlesRouter);
