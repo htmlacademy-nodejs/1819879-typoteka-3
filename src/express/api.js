e43c7dd03438eba712feaa45e96c2b460d6606fa
Roman Shestakov <shestakov_rv@bk.ru>
2022-07-26 21:06:20 +0300

    Added class api from front

---
+++ src/express/api.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const axios = require(`axios`);
+const TIMEOUT = 1000;
+const port = process.env.API_PORT || 3000;
+const defaultUrl = `http://localhost:${port}/api/`;
+
+
+class API {
+  constructor(baseURL, timeout) {
+    this._http = axios.create({
+      baseURL,
+      timeout
+    });
+  }
+
+  async _load(url, options) {
+    const response = await this._http.request({url, ...options});
+    return response.data;
+  }
+
+  getArticles() {
+    return this._load(`/articles`);
+  }
+
+  getArticle(id) {
+    return this._load(`/articles/${id}`);
+  }
+
+  createArticle({articleData}) {
+    return this._load(`/article`, {
+      method: `post`,
+      data: articleData
+    });
+  }
+
+  getCategories() {
+    return this._load(`/categories`);
+  }
+}
+
+const defaultAPI = new API(defaultUrl, TIMEOUT);
+
+module.exports = {
+  API,
+  getAPI: () => defaultAPI
+};
