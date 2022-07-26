27efa19077f77fcde933fdd2f63f50af5ef26263
Roman Shestakov <shestakov_rv@bk.ru>
2022-07-26 21:08:23 +0300

    Added articles in main page

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,12 +1,21 @@
 'use strict';
 
 const {Router} = require(`express`);
+const api = require(`./../api`).getAPI();
 
 const mainRouter = new Router();
-mainRouter.get(`/`, (req, res) => res.render(`pages/main`));
+
+mainRouter.get(`/`, async (req, res) => {
+  const articles = await api.getArticles();
+  res.render(`pages/main`, {articles});
+});
+
 mainRouter.get(`/register`, (req, res) => res.render(`pages/sign-up`));
+
 mainRouter.get(`/login`, (req, res) => res.render(`pages/login`));
+
 mainRouter.get(`/search`, (req, res) => res.render(`pages/search`));
+
 mainRouter.get(`/categories`, (req, res) => res.render(`pages/all-categories`));
 
 module.exports = mainRouter;
