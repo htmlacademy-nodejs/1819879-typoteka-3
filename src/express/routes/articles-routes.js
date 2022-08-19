2207344600ff0f55895b87ad18976ada1ba16145
Roman Shestakov <shestakov_rv@bk.ru>
2022-08-19 17:13:03 +0300

    Added data for edit article page

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -1,11 +1,20 @@
 'use strict';
 
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
 
 const articlesRouter = new Router();
 articlesRouter.get(`/add`, (req, res) => res.render(`pages/articles/post`));
 articlesRouter.get(`/:id`, (req, res) => res.render(`pages/articles/post-detail`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.render(`pages/articles/post`));
+articlesRouter.get(`/edit/:id`, async (req, res) => {
+  const articleId = req.params.id;
+  const [article, categories] = await Promise.all([
+    api.getArticle(articleId),
+    api.getCategories()
+  ]);
+
+  res.render(`pages/articles/post`, {article, categories});
+});
 articlesRouter.get(`/category/:id`, (req, res) => res.render(`pages/articles/articles-by-category`));
 
 module.exports = articlesRouter;
