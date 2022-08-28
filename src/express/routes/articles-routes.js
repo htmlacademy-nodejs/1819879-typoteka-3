93b5e25a31411468c79caade1ab9fc97cf2d7ad1
Roman Shestakov <shestakov_rv@bk.ru>
2022-08-28 19:10:11 +0300

    Separated post template to edit and new

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -4,8 +4,16 @@ const {Router} = require(`express`);
 const api = require(`../api`).getAPI();
 
 const articlesRouter = new Router();
-articlesRouter.get(`/add`, (req, res) => res.render(`pages/articles/post`));
-articlesRouter.get(`/:id`, (req, res) => res.render(`pages/articles/post-detail`));
+articlesRouter.get(`/add`, async (req, res) => {
+  const categories = await api.getCategories();
+
+  return res.render(`pages/articles/post-new`, {categories});
+});
+
+articlesRouter.get(`/:id`, (req, res) => {
+  return res.render(`pages/articles/post-detail`);
+});
+
 articlesRouter.get(`/edit/:id`, async (req, res) => {
   const articleId = req.params.id;
   const [article, categories] = await Promise.all([
@@ -13,8 +21,9 @@ articlesRouter.get(`/edit/:id`, async (req, res) => {
     api.getCategories()
   ]);
 
-  res.render(`pages/articles/post`, {article, categories});
+  res.render(`pages/articles/post-edit`, {article, categories});
 });
+
 articlesRouter.get(`/category/:id`, (req, res) => res.render(`pages/articles/articles-by-category`));
 
 module.exports = articlesRouter;
