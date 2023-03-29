2e3b57400dfeb271d454d08858f5969138ca4cfc
Roman Shestakov <shestakov_rv@bk.ru>
2023-03-29 19:43:58 +0300

    Separation startup server and connect to database

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -10,49 +10,57 @@ const DEFAULT_PORT = 3000;
 const TEXT_NOT_FOUND = `Not found`;
 
 const logger = getLogger({name: `api`});
-const server = express();
-server.use(express.json({limit: MAX_REQUEST_SIZE}));
 
-server.use((req, res, next) => {
-  logger.debug(`Requested route api - ${req.url}`);
-  res.on(`finish`, () => {
-    logger.info(`Response status code - ${res.statusCode}`);
+const connectDataBase = async () => {
+  try {
+    logger.info(`Trying to connect to database...`);
+    await sequelize.authenticate();
+  } catch (err) {
+    logger.error(`An error occurred connect database: ${err.message}`);
+    process.exit(1);
+  }
+  logger.info(`Connection to database established`);
+};
+
+const runHttpServer = async (port) => {
+  const server = express();
+  server.use(express.json({limit: MAX_REQUEST_SIZE}));
+
+  server.use((req, res, next) => {
+    logger.debug(`Requested route api - ${req.url}`);
+    res.on(`finish`, () => {
+      logger.info(`Response status code - ${res.statusCode}`);
+    });
+    next();
   });
-  next();
-});
 
-server.use(`/api`, routeApp);
+  server.use(`/api`, routeApp);
 
-server.use(`/`, (req, res) => {
-  res.status(HttpCode.NOT_FOUND).send(TEXT_NOT_FOUND);
-  logger.error(`Route not found - ${req.url}`);
-});
+  server.use(`/`, (req, res) => {
+    res.status(HttpCode.NOT_FOUND).send(TEXT_NOT_FOUND);
+    logger.error(`Route not found - ${req.url}`);
+  });
 
-server.use((err, _req, res, _next) => {
-  logger.error(`An error occurred on processing request: ${err.stack}`);
-  res.status(HttpCode.INTERNAL_SERVER_ERROR).send(`Internal error`);
-});
+  server.use((err, _req, res, _next) => {
+    logger.error(`An error occurred on processing request: ${err.stack}`);
+    res.status(HttpCode.INTERNAL_SERVER_ERROR).send(`Internal error`);
+  });
+
+  server.listen(port, (error) => {
+    if (error) {
+      logger.error(`Error started server ${error.message}`);
+      return;
+    }
+    logger.info(`Server started in ${port} port`);
+  });
+};
 
 module.exports = {
   name: `--server`,
   async run(customPort) {
-    try {
-      logger.info(`Trying to connect to database...`);
-      await sequelize.authenticate();
-    } catch (err) {
-      logger.error(`An error occurred: ${err.message}`);
-      process.exit(1);
-    }
-    logger.info(`Connection to database established`);
+    await connectDataBase();
 
     const port = Number.parseInt(customPort, DEFAULT_RADIX) || DEFAULT_PORT;
-
-    server.listen(port, (error) => {
-      if (error) {
-        logger.error(`Error started server ${error.message}`);
-        return;
-      }
-      logger.info(`Server started in ${port} port`);
-    });
+    await runHttpServer(port);
   }
 };
